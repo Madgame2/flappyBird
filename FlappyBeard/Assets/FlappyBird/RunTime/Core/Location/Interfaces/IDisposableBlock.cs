7d2b9efd3da3e7330a3fa15cf378f40a2c01e468
Anton <astakheyko@bk.ru>
2026-06-26 13:11:33 +0300

    Remov unuse objects

--- FlappyBeard/Assets/FlappyBird/RunTime/Core/Location/Interfaces/IDisposableBlock.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace FlappyBird.Runtime.Core.Location.Interfaces
-{
-    public interface IDisposableBlock
-    {
-        event Action<IDisposableBlock> OnRequestRelease;
-    }
-}
--- FlappyBeard/Assets/FlappyBird/RunTime/Core/Services/GameObjectType.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public enum GameObjectType
-{
-    Player,
-    LocationBlock
-}
